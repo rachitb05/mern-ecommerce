48d5dbc35a1ae3242cba670fe87bd30de2d15b59
rachitb05 <mr.rachitb@gmail.com>
2022-07-11 03:28:31 +0530

    4.Backend Setup Complete

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+dotenv.config();
+connectDB();
+const app = express();
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link ,useParams} from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id } = useParams();
-  console.log(id);
-  const product = products.find((p) => p._id === id);
+ 
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    }; 
+    fetchProduct();
+  }, [id]);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
