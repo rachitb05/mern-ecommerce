4e4c07a7a64f9d2c8bd350abfd77c45ef5882813
rachitb05 <mr.rachitb@gmail.com>
2022-08-01 21:07:20 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ app.get("/api/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV.length());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
